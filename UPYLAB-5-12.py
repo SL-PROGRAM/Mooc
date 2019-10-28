f4e7d3c3238f1e16f8c22662f9e448f1a57b0166
sl-prog <s.leyral@gmail.com>
2019-10-28 21:07:40 +0100

    Add UPYLAB 5-12

--- UPYLAB-5-12.py
+++ UPYLAB-5-12.py
@@ -4,7 +4,10 @@
 
 Enoncé
 
+L’exercice est le même que le précédent, mais ici, si les paramètres ont le type attendu, la fonction modifie la liste
+en place et ne retourne rien. Si les paramètres ne sont pas valides, une erreur se produit à l’exécution.
 
+Énonce précedent:
 
 Écrire une fonction my_insert qui reçoit comme premier paramètre une liste triée d’entiers par ordre croissant et comme
 deuxième paramètre un entier n, et qui renvoie une liste correspondant à la liste reçue, mais dans laquelle le nombre
@@ -27,18 +30,14 @@ particulier aucun appel à input ou à print.
 
 """
 
+
 def my_insert(liste, n):
-    new_liste = liste.copy()
-    if type(liste) != list or type(n) != int:
-        new_liste = None
-    else:
-        for i in liste:
-            if i > n:
-                pos = liste.index(i)
-                new_liste.insert(pos, n)
-                break
-    return new_liste
-
-print(my_insert([1,2,4],3))
+    assert type(liste) == list or type(n) == int
+    for i in liste:
+        if i > n:
+            pos = liste.index(i)
+            liste.insert(pos, n)
+            break
 
 
+print(my_insert([1, 2, 4], 3))
